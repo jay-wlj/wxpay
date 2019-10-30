840814cd08dbbed414e88c463fb012db936f0252
jay <305898636@qq.com>
2019-10-30 14:27:36 +0800

    add some funcitons

--- params.go
+++ params.go
@@ -1,6 +1,10 @@
 package wxpay
 
-import "strconv"
+import (
+	"encoding/xml"
+	"io"
+	"strconv"
+)
 
 type Params map[string]string
 
@@ -29,4 +33,58 @@ func (p Params) GetInt64(k string) int64 {
 func (p Params) ContainsKey(key string) bool {
 	_, ok := p[key]
 	return ok
+}
+
+// 判断是否成功
+func (p Params) IsSuccess() bool {
+	if p.ContainsKey("return_code") {
+		returnCode := p.GetString("return_code")
+		if returnCode == Success {
+			if p.ContainsKey("result_code") {
+				resultCode := p.GetString("result_code")
+				return resultCode == Success
+			}
+		}
+	}
+	return false
+}
+
+type xmlMapEntry struct {
+	XMLName xml.Name
+	Value   string `xml:",chardata"`
+}
+
+func (m Params) MarshalXML(e *xml.Encoder, start xml.StartElement) error {
+	if len(m) == 0 {
+		return nil
+	}
+
+	start.Name.Local = "xml" // 更新开始标签为xml
+	err := e.EncodeToken(start)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range m {
+		e.Encode(xmlMapEntry{XMLName: xml.Name{Local: k}, Value: v})
+	}
+
+	return e.EncodeToken(start.End())
+}
+
+func (m *Params) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	*m = Params{}
+	for {
+		var e xmlMapEntry
+
+		err := d.Decode(&e)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+
+		(*m)[e.XMLName.Local] = e.Value
+	}
+	return nil
 }
